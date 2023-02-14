72d82eb7dc3308fa508227a585107be246e70941
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2023-02-14 14:42:16 +0300

    gamemanager script and other settings

---
+++ PICKER/Assets/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject PickerObj;
+    [SerializeField] private bool PickerMoveSituation;
+
+
+    void Start()
+    {
+        PickerMoveSituation = true;
+    }
+
+    
+    void Update()
+    {
+        if (PickerMoveSituation)
+        {
+            PickerObj.transform.position += 4f * Time.deltaTime * PickerObj.transform.forward;
+
+            if (Time.timeScale!=0)
+            {
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    PickerObj.transform.position = Vector3.Lerp(PickerObj.transform.position,
+                        new Vector3(PickerObj.transform.position.x -.1f,PickerObj.transform.position.y,PickerObj.transform.position.z),.05f);
+                }
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    PickerObj.transform.position = Vector3.Lerp(PickerObj.transform.position,
+                        new Vector3(PickerObj.transform.position.x  +.1f, PickerObj.transform.position.y, PickerObj.transform.position.z),.05f);
+                }
+            }
+        }
+    }
+}
