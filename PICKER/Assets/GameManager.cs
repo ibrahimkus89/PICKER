cb5a313f9fbf609148c8396c3814bb10f37d18ee
IBRAHIM KUS <80965145+ibrahimkus89@users.noreply.github.com>
2023-02-15 10:08:27 +0300

    picker extra feature

--- PICKER/Assets/GameManager.cs
+++ PICKER/Assets/GameManager.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using TMPro;
+using Unity.VisualScripting;
+
+[Serializable]
+
+public class BallFieldProcess
+{
+    public Animator BallFieldAnimator;
+    public TextMeshProUGUI numberText;
+    public int AgBall;
+}
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject PickerObj;
+    [SerializeField] private GameObject BallControlObj;
     [SerializeField] private bool PickerMoveSituation;
 
-
+    private int AtlBallNumber;
+    [SerializeField] private List<BallFieldProcess> _BallFieldProcess =new List<BallFieldProcess>();
     void Start()
     {
         PickerMoveSituation = true;
+        _BallFieldProcess[0].numberText.text = AtlBallNumber + "/" + _BallFieldProcess[0].AgBall;
     }
 
     
@@ -41,6 +56,24 @@ public class GameManager : MonoBehaviour
     public void BorderReach()
     {
         PickerMoveSituation=false;
-        Debug.Log("here");
+
+        Collider[] hitCol = Physics.OverlapBox(BallControlObj.transform.position,BallControlObj.transform.localScale / 2,Quaternion.identity);
+
+        int i = 0;
+        while (i<hitCol.Length)
+        {
+
+            hitCol[i].GetComponent<Rigidbody>().AddForce(new Vector3(0,0,.8f),ForceMode.Impulse);
+
+            i++;
+        }
+
+        Debug.Log(i);
     }
+
+    //void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireCube(BallControlObj.transform.position,BallControlObj.transform.localScale);
+    //}
 }
